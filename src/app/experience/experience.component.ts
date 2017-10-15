04b7e1a1ce7eba4556d45a7fcf0a5c2cea31cb16
yidah <yidahit@hotmail.com>
2017-10-15 21:25:47 +0100

    Font changes

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
-    templateUrl: 'experience.component.html'
+    templateUrl: 'experience.component.html',
+    styleUrls:['experience.component.css']
 })
 export class ExperienceComponent {
 
